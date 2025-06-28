5ffd826d189b52a3bc7ef165585ccc16f785d1f6
Juniven Saavedra <junivensaavedra@gmail.com>
2025-06-28 22:56:06 +0800

    Implement adieu function to generate farewell message based on input names

---
+++ adieu/adieu.py
@@ -0,0 +1,20 @@
+input_names = []
+
+
+def adieu() -> None:
+	final_str = "Adieu, adieu, to "
+
+	if len(input_names) == 1:
+		final_str += f"{input_names[0]}"
+	elif len(input_names) == 2:
+		final_str += f"{input_names[0]} and {input_names[1]}"
+	else:
+		for idx, name in enumerate(input_names):
+			if len(input_names) > 2:
+				last_element = input_names[-1]	
+				if name == last_element:
+					final_str += f"and {name}"
+				else:
+					final_str += f"{name}, "
+		
+	print(final_str)
