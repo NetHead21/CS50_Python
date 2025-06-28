aa558aad37436da197974b3824c4531445ce29bc
Juniven Saavedra <junivensaavedra@gmail.com>
2025-06-28 22:59:15 +0800

    Fix indentation in bank.py for consistent formatting

--- bank/bank.py
+++ bank/bank.py
@@ -1,10 +1,11 @@
 def bank(greetings: str) -> None:
-	if greetings == "hello":
-		print("$0")
-	elif greetings[0] == "h":
-		print("$20")
-	else:
-		print("$100")
+    if greetings == "hello":
+        print("$0")
+    elif greetings[0] == "h":
+        print("$20")
+    else:
+        print("$100")
 
-greetings = input("Input Greeting: ").lower().split()[0].replace(",","")
+
+greetings = input("Input Greeting: ").lower().split()[0].replace(",", "")
 bank(greetings)
