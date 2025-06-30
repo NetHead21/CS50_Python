c083c2b3ddaef2b0e1cbecbfcef59ba543d0e12e
Juniven Saavedra <junivensaavedra@gmail.com>
2025-06-30 22:42:44 +0800

    Add get_bitcoin_price function to fetch current Bitcoin price from CoinDesk API

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,3 +1,6 @@
 import sys
 import requests
 
+def get_bitcoin_price() -> float:
+    """Fetch the current Bitcoin price in USD from CoinDesk API."""
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
