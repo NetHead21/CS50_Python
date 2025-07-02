cb9b8261dced16afc86b4afc3e1d2951c5168733
Juniven Saavedra <junivensaavedra@gmail.com>
2025-07-02 21:35:40 +0800

    Fix formatting issues by adding blank lines for better readability

--- camel/camel.py
+++ camel/camel.py
@@ -1,9 +1,11 @@
 import re
 
+
 def camel(variable_name: str) -> None:
     name_list = re.findall(".[^A-Z]*", variable_name)
     final_variable_name = "_".join(name_list).lower()
     print(final_variable_name)
+
 
 variable_name = input("Enter a variable name: ")
 camel(variable_name)
