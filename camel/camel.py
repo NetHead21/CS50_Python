704e7d99a8ea4d3b45224be570707873ce2f9af1
Juniven Saavedra <junivensaavedra@gmail.com>
2025-07-02 21:29:59 +0800

    Implement camel case conversion function in camel.py

--- camel/camel.py
+++ camel/camel.py
@@ -1,1 +1,6 @@
 import re
+
+def camel(variable_name: str) -> None:
+    name_list = re.findall(".[^A-Z]*", variable_name)
+    final_variable_name = "_".join(name_list).lower()
+    print(final_variable_name)
